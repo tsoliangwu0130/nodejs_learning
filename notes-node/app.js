75cc68900d6aa6719ec6dd6a2c9684d47564672e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-06 22:50:17 -0700

    Practice require modules

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,1 +1,5 @@
-console.log("starting notes-node application");
+console.log("Starting notes-node application.");
+
+// require fs module
+const fs = require('fs');
+fs.appendFile('greetings.txt', "Hello from Node.js!");
