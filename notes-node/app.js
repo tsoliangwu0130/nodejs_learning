f202cd091cdd03b4eacf032eacfeba2bb57e24b7
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-07 23:13:20 -0700

    Some lodash practice

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,9 +2,10 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var user = os.userInfo();
-var res = notes.addNote();
-
-fs.appendFile('greetings.txt', `Hello ${ user.username }! You are ${ notes.age }.`);
+var filteredArray = _.uniq(['Tso-Liang', 1, 'Wu', 'Tso-Liang', 2, 1, 3]);
+console.log(filteredArray);
+console.log(_.isString(filteredArray));
+console.log(_.isString('Tso-Liang Wu'));
