229afca889409b401354401fb7b8a07ce2f53834
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-08 11:06:46 -0700

    Clean code

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,11 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
-const notes = require('./notes.js');
 
-var filteredArray = _.uniq(['Tso-Liang', 1, 'Wu', 'Tso-Liang', 2, 1, 3]);
-console.log(filteredArray);
-console.log(_.isString(filteredArray));
-console.log(_.isString('Tso-Liang Wu'));
+const notes = require('./notes.js');
