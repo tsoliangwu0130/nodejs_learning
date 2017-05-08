5d0734e39182658087ee47ee1f576fd0b4d3b7d6
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-08 15:23:45 -0700

    Implement addNote

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,5 +1,24 @@
+const fs = require('fs');
+
 var addNote = (title, body) => {
-    console.log('Adding note:', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        var noteString = fs.readFileSync('note-data.json');
+        notes = JSON.parse(noteString);
+    } catch (e) {
+        // console.log(e);
+    }
+
+    var duplicateNotes = notes.filter((note) => note.title === title); // ES6 style
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        fs.writeFileSync('note-data.json', JSON.stringify(notes));
+    }
 };
 
 var removeNote = (title) => {
