f55779dc48b5899375d885eed1bfd6c7513f5c4f
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-09 22:25:54 -0700

    Initial weather app

---
+++ weather-node/app.js
@@ -0,0 +1,11 @@
+console.log('Start app...');
+
+setTimeout(() => {
+    console.log('Inside of callback...');
+}, 2000);
+
+setTimeout(() => {
+    console.log('Second setTimeout...');
+}, 0);
+
+console.log('Finish up app...');
