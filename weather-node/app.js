40487acf5967283277879c7bf11ff2ea24a0cafd
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-10 20:50:12 -0700

    Get user's input

--- weather-node/app.js
+++ weather-node/app.js
@@ -1,4 +1,18 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true // always parse arguments to string
+        }
+    })
+    .help()
+    .alias('h', 'help')
+    .argv;
 
 request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Oregon%20State%20University',
