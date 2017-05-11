4dd3c9389b71e56e8d95fb37dd25ce5522458212
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-10 20:33:15 -0700

    Obtain lat and lng data from response

--- weather-node/app.js
+++ weather-node/app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=2201%20NW%20Grant%20Ave%20corvallis',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=oregon%20state%20university',
     json: true
 }, (error, response, body) => {
-    console.log(JSON.stringify(body, undefined, 4)); // pretty print json object
+    console.log(`Address: ${ body.results[0].formatted_address }`);
+    console.log(`Latitude: ${ body.results[0].geometry.location.lat }`);
+    console.log(`Longitude: ${ body.results[0].geometry.location.lng }`);
 });
