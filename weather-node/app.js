b5964f0281fa466220d0c85c93ea4bc83ddf226e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-11 11:53:30 -0700

    Refactor codes

--- weather-node/app.js
+++ weather-node/app.js
@@ -1,5 +1,6 @@
-const request = require('request');
 const yargs = require('yargs');
+
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -14,19 +15,4 @@ const argv = yargs
     .alias('h', 'help')
     .argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
-
-request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${ encodedAddress }`,
-    json: true
-}, (error, response, body) => {
-    if (error) {
-        console.log('Unable to connect to Google server.');
-    } else if (body.status === 'ZERO_RESULTS') {
-        console.log('Unalbe to find the address.');
-    } else if (body.status === 'OK') {
-        console.log(`Address: ${ body.results[0].formatted_address }`);
-        console.log(`Latitude: ${ body.results[0].geometry.location.lat }`);
-        console.log(`Longitude: ${ body.results[0].geometry.location.lng }`);
-    }
-});
+geocode.geocodeAddress(argv.address);
