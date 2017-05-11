25381e1e6dc80a6ad64966990b95f8f23b4c2ae0
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-10 17:51:46 -0700

    Fix ESling warning

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -4,61 +4,61 @@ const noteFile = 'note-data.json';
 
 var fetchNotes = () => {
   // prevent the app from crashing if the noteFile doesn't exist
-  try {
-    var noteString = fs.readFileSync(noteFile);
-    return JSON.parse(noteString);
-  } catch (e) {
-    return [];
-  }
+    try {
+        var noteString = fs.readFileSync(noteFile);
+        return JSON.parse(noteString);
+    } catch (e) {
+        return [];
+    }
 };
 
 var saveNotes = (notes) => {
-  fs.writeFileSync(noteFile, JSON.stringify(notes));
+    fs.writeFileSync(noteFile, JSON.stringify(notes));
 };
 
 var addNote = (title, body) => {
-  var notes = fetchNotes();
-  var note = {
-    title,
-    body
-  };
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
   // avoid creating duplicated notes
-  var duplicateNotes = notes.filter((note) => note.title === title); // ES6 style
-  if (duplicateNotes.length === 0) {
-    notes.push(note);
-    saveNotes(notes);
-    return note;
-  }
+    var duplicateNotes = notes.filter((note) => note.title === title); // ES6 style
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
 };
 
 var removeNote = (title) => {
-  var notes = fetchNotes();
-  var filteredNotes = notes.filter((note) => note.title !== title);
-  saveNotes(filteredNotes);
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title !== title);
+    saveNotes(filteredNotes);
 
-  return notes.length !== filteredNotes.length;
+    return notes.length !== filteredNotes.length;
 };
 
 var getNote = (title) => {
-  var notes = fetchNotes();
-  var filteredNotes = notes.filter((note) => note.title === title);
-  return filteredNotes[0];
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
 };
 
 var getAll = () => {
-  return fetchNotes();
+    return fetchNotes();
 };
 
 var logNote = (note) => {
-  console.log('----');
-  console.log(`Title: ${ note.title }`);
-  console.log(`Body: ${ note.body }`);
+    console.log('----');
+    console.log(`Title: ${ note.title }`);
+    console.log(`Body: ${ note.body }`);
 };
 
 module.exports = {
-  addNote,
-  removeNote,
-  getNote,
-  getAll,
-  logNote
+    addNote,
+    removeNote,
+    getNote,
+    getAll,
+    logNote
 };
