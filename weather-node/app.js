1fa3df26a08d001289e1fdb93124263ac0d48ff7
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-10 17:54:35 -0700

    Fix eslint warning

--- weather-node/app.js
+++ weather-node/app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=2201%20NW%20Grant%20Ave%20corvallis',
-  json: true
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=2201%20NW%20Grant%20Ave%20corvallis',
+    json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 4)); // pretty print json object
+    console.log(JSON.stringify(body, undefined, 4)); // pretty print json object
 });
