05c39c0857a009de7e3cf5eab554e7060153b322
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-10 20:38:42 -0700

    Install yargs

--- weather-node/app.js
+++ weather-node/app.js
@@ -1,7 +1,7 @@
 const request = require('request');
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=oregon%20state%20university',
+    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=Oregon%20State%20University',
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${ body.results[0].formatted_address }`);
