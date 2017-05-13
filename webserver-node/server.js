afc955b01cbd880d199d32b77f54c25c6589df3d
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-13 00:40:14 -0700

    Render template with hbs

--- webserver-node/server.js
+++ webserver-node/server.js
@@ -1,19 +1,25 @@
 /* global __dirname */
 
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Express!</h1>');
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Hello Express!'
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send({
-        name: 'Tso-Liang Wu',
-        school: 'OSU'
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
     });
 });
 
