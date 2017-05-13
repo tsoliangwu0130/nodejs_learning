f39e6e27a043f82e745724b8f68d944ac970c728
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-13 16:57:14 -0700

    Add new middleware

--- webserver-node/server.js
+++ webserver-node/server.js
@@ -1,13 +1,27 @@
 /* global __dirname */
 
 const express = require('express');
+const fs = require('fs');
 const hbs = require('hbs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+// express middleware
 app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${ now }: ${ req.method } ${ req.url }`;
+    console.log(log);
+    fs.appendFile('server.log', `${ log }\n`, (err) => {
+        if (err) {
+            console.log('Unable to append to server.log.');
+        }
+    });
+    next();
+});
 
 // helper functions
 hbs.registerHelper('getCurrentYear', () => {
