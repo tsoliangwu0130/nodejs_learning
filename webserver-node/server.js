5be70f49a550b62ef6b09dddef365d23bd65c3b4
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-13 14:39:39 -0700

    Improve code with advanced template

--- webserver-node/server.js
+++ webserver-node/server.js
@@ -5,13 +5,23 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+
+// helper functions
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
-        currentYear: new Date().getFullYear(),
         welcomeMessage: 'Hello Express!'
     });
 });
@@ -19,7 +29,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page',
-        currentYear: new Date().getFullYear()
     });
 });
 
