57ecf83f00f6fec2a83f65491fe44d9efadfa81e
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-14 12:59:28 -0700

    Mock unit testing

--- tests-node/spies/app.test.js
+++ tests-node/spies/app.test.js
@@ -1,11 +1,28 @@
 /* eslint-env mocha */
 
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
 
 describe('App', () => {
+    // mock db
+    var db = {
+        saveUser: expect.createSpy()
+    };
+    app.__set__('db', db);
+
     it('should call the spy correctly', () => {
         var spy = expect.createSpy();
         spy('Leon', 28);
         expect(spy).toHaveBeenCalledWith('Leon', 28);
+    });
+
+    it('should call saveUser with user object', () => {
+        var email = 'tsoliangwu0130@gmail.com';
+        var password = '123abc';
+
+        app.handleSignup(email, password);
+        expect(db.saveUser).toHaveBeenCalledWith({ email, password });
     });
 });
