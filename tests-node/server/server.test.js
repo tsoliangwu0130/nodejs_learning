66cdb137b2572a92d2d594aaf30653ebd3b031fa
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-14 12:35:38 -0700

    Organizing test case with describe()

--- tests-node/server/server.test.js
+++ tests-node/server/server.test.js
@@ -5,29 +5,36 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-// test express application
-it('should return 404 response', (done) => {
-    request(app)
-        .get('/')
-        .expect(404)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.',
-                name: 'Todo App v1.0'
-            });
-        })
-        .end(done);
-});
+describe('Server', () => {
+    // test express application
+    describe('#GET /', () => {
 
-it('should return my user object', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Furball',
-                age: 1
-            });
-        })
-        .end(done);
+        it('should return 404 response', (done) => {
+            request(app)
+                .get('/')
+                .expect(404)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.',
+                        name: 'Todo App v1.0'
+                    });
+                })
+                .end(done);
+        });
+    });
+
+    describe('#GET /users', () => {
+        it('should return my user object', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Furball',
+                        age: 1
+                    });
+                })
+                .end(done);
+        });
+    });
 });
--- tests-node/utils/utils.test.js
+++ tests-node/utils/utils.test.js
@@ -4,57 +4,63 @@ const expect = require('expect');
 
 const utils = require('./utils');
 
-// test some functions
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
+describe('Utils', () => {
+    // test some functions
+    describe('#some functions', () => {
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
 
-    expect(res).toBe(44).toBeA('number');
-});
+            expect(res).toBe(44).toBeA('number');
+        });
 
-it('should squre a number', () => {
-    var res = utils.square(9);
+        it('should squre a number', () => {
+            var res = utils.square(9);
 
-    expect(res).toBe(81).toBeA('number');
-});
+            expect(res).toBe(81).toBeA('number');
+        });
 
-it('should expect some values', () => {
-    expect(12).toNotBe(11);
-    expect({ name: 'Leon' }).toNotEqual({ name: 'leon' }); // objects check
-    expect([1, 2, 3]).toInclude(3);
-    expect([1, 2, 3]).toExclude(5);
-    expect({
-        name: 'Leon',
-        age: 28,
-        location: 'Corvallis'
-    }).toInclude({
-        age: 28
+        it('should expect some values', () => {
+            expect(12).toNotBe(11);
+            expect({ name: 'Leon' }).toNotEqual({ name: 'leon' }); // objects check
+            expect([1, 2, 3]).toInclude(3);
+            expect([1, 2, 3]).toExclude(5);
+            expect({
+                name: 'Leon',
+                age: 28,
+                location: 'Corvallis'
+            }).toInclude({
+                age: 28
+            });
+        });
+
+        it('should set first and last names', () => {
+            var user = {
+                location: 'Corvallis',
+                age: 28
+            };
+            var res = utils.setName(user, 'Tso-Liang Wu');
+
+            expect(res).toInclude({
+                firstName: 'Tso-Liang',
+                lastName: 'Wu'
+            });
+        });
     });
-});
 
-it('should set first and last names', () => {
-    var user = {
-        location: 'Corvallis',
-        age: 28
-    };
-    var res = utils.setName(user, 'Tso-Liang Wu');
+    // test async functions
+    describe('#async functions', () => {
+        it('should async add two numbers', (done) => {
+            utils.asyncAdd(4, 3, (sum) => {
+                expect(sum).toBe(7).toBeA('number');
+                done();
+            });
+        });
 
-    expect(res).toInclude({
-        firstName: 'Tso-Liang',
-        lastName: 'Wu'
-    });
-});
-
-// test async functions
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
-    });
-});
-
-it('should async square a number', (done) => {
-    utils.asyncSquare(5, (sqr) => {
-        expect(sqr).toBe(25).toBeA('number');
-        done();
+        it('should async square a number', (done) => {
+            utils.asyncSquare(5, (sqr) => {
+                expect(sqr).toBe(25).toBeA('number');
+                done();
+            });
+        });
     });
 });
