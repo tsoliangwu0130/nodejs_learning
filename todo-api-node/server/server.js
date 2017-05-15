ab4767c45d413d73899118080594ced2b17529b6
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-15 16:13:36 -0700

    Intial server with mongoose

--- todo-api-node/server/server.js
+++ todo-api-node/server/server.js
@@ -1,80 +1,33 @@
 const mongoose = require('mongoose');
-const { MongoClient, ObjectID } = require('mongodb'); // destructure an object - ES6
 
 var dbURL = 'mongodb://localhost:27017/TodoApp';
 
-// connect to database
-MongoClient.connect(dbURL, (err, db) => {
-    if (err) {
-        return console.log('Unable to connect to MongoDB server.');
+// connect MongoDB use mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect(dbURL);
+
+// create collection model (schema)
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String
+    },
+    completed: {
+        type: Boolean
+    },
+    completedAt: {
+        type: Number
     }
-    console.log('Connected to MongoDB server.');
+});
 
-    // insert document to collection
-    // db.collection('Todos').insertOne({
-    //     text: 'Something to do',
-    //     completed: false
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert todo:', err);
-    //     }
-    //     console.log(JSON.stringify(result.ops, undefined, 4));
-    // });
+var newTodo = new Todo({
+    text: 'Eat dinner',
+    completed: true,
+    completedAt: 123
+});
 
-    // db.collection('Users').insertOne({
-    //     name: 'Leon',
-    //     age: 28,
-    //     location: 'Corvallis'
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert users:', err);
-    //     }
-    //     console.log(JSON.stringify(result.ops, undefined, 4));
-    // });
-
-    // fetch documents from collection
-    // db.collection('Todos').find({
-    //     _id: new ObjectID('591a0608fe88016df4971871')
-    // }).toArray().then((docs) => {
-    //     console.log('Todos:');
-    //     console.log(JSON.stringify(docs, undefined, 4));
-    // }, (err) => {
-    //     console.log('Unable to fetch todos:', err);
-    // });
-
-    // db.collection('Todos').find().count().then((count) => {
-    //     console.log(`Todos count: ${ count } `);
-    // }, (err) => {
-    //     console.log('Unable to fetch todos:', err);
-    // });
-
-    // delete documents from collection
-    // db.collection('Todos').deleteMany({ text: 'Excerise' }).then((result) => {
-    //     console.log(result);
-    // });
-
-    // db.collection('Todos').deleteOne({ text: 'Excerise' }).then((result) => {
-    //     console.log(result);
-    // });
-
-    // db.collection('Todos').findOneAndDelete({ completed: false }).then((result) => {
-    //     console.log(result);
-    // });
-
-    // update documents from collection
-    db.collection('Todos').findOneAndUpdate({
-        _id: new ObjectID('591a0608fe88016df4971871')
-    }, {
-        // MongoDB update operator
-        $set: {
-            completed: true
-        }
-    }, {
-        returnOriginal: false
-    }).then((result) => {
-        console.log(result);
-    });
-
-    // close database
-    // db.close();
+// save documents to collection
+newTodo.save().then((doc) => {
+    console.log('Save todo:', doc);
+}, (err) => {
+    console.log('Unable to save todo:', err);
 });
