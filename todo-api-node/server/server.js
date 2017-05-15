0c10f01e1e666122cede6e7c9dd2fd9283ade573
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-15 16:31:46 -0700

    Add user collection

--- todo-api-node/server/server.js
+++ todo-api-node/server/server.js
@@ -9,25 +9,48 @@ mongoose.connect(dbURL);
 // create collection model (schema)
 var Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true // remove leading or trailing spaces
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false,
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+// create some sample documents
 var newTodo = new Todo({
-    text: 'Eat dinner',
-    completed: true,
-    completedAt: 123
+    text: '  hello'
+});
+
+var newUser = new User({
+    email: '  tsoliangwu0130@gmail.com '
 });
 
 // save documents to collection
 newTodo.save().then((doc) => {
-    console.log('Save todo:', doc);
+    console.log(JSON.stringify(doc, undefined, 4));
 }, (err) => {
     console.log('Unable to save todo:', err);
+});
+
+newUser.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 4));
+}, (err) => {
+    console.log('Unable to save user:', err);
 });
