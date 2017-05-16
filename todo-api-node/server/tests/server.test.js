4ce36fe7d2033f8d1c9da681cf54a3815c334e4b
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-15 21:55:44 -0700

    Add POST unit test for server

---
+++ todo-api-node/server/tests/server.test.js
@@ -0,0 +1,53 @@
+/* eslint-env mocha */
+
+const expect = require('expect');
+const request = require('supertest');
+
+const { app } = require('./../server');
+const { Todo } = require('./../models/todo');
+
+// run before every test case
+beforeEach((done) => {
+    Todo.remove({}).then(() => done()); // empty database
+});
+
+describe('POST /todos', () => {
+    it('should create a new todo', (done) => {
+        var text = 'Test todo text';
+
+        request(app)
+            .post('/todos')
+            .send({ text })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+
+    it('should not create todo with invalid body data', (done) => {
+        request(app)
+            .post('/todos')
+            .send({})
+            .expect(400)
+            .end((err) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.find().then((todo) => {
+                    expect(todo.length).toBe(0);
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+});
