28d02a60bf16e30e04f1a29162bac568a2160e0b
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-17 15:42:37 -0700

    Get ready to deploy to Heroku

--- todo-api-node/server/db/mongoose.js
+++ todo-api-node/server/db/mongoose.js
@@ -1,9 +1,11 @@
+/* global process */
+
 const mongoose = require('mongoose');
 
 var dbURL = 'mongodb://localhost:27017/TodoApp';
 
 // connect MongoDB use mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect(dbURL);
+mongoose.connect(process.env.MONGODB_URI || dbURL);
 
 module.exports = { mongoose };
--- todo-api-node/server/server.js
+++ todo-api-node/server/server.js
@@ -9,7 +9,7 @@ const { Todo } = require('./models/todo');
 const { User } = require('./models/user');
 
 var app = express();
-const port = process.env.PORT() || 3000;
+const port = process.env.PORT || 3000;
 
 // middleware
 app.use(bodyParser.json());
