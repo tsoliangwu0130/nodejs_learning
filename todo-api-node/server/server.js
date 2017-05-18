03ad38246df8802ed3270b0d640b08e16b9fdf13
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-17 21:29:26 -0700

    Add unit test for delete method

--- todo-api-node/server/server.js
+++ todo-api-node/server/server.js
@@ -65,7 +65,7 @@ app.delete('/todos/:id', (req, res) => {
         if (!todo) {
             return res.status(404).send();
         }
-        res.send(todo);
+        res.send({ todo });
     }).catch((err) => {
         res.status(400).send(err);
     });
