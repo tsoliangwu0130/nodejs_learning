0240c4f61b2dd9a4acc2c3666c48fb38cd6abc57
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-18 15:12:52 -0700

    Add unit test for PATCH method

--- todo-api-node/server/tests/server.test.js
+++ todo-api-node/server/tests/server.test.js
@@ -13,7 +13,9 @@ const todos = [{
     text: 'First test todo'
 }, {
     _id: new ObjectID(),
-    text: 'Second test todo'
+    text: 'Second test todo',
+    completed: true,
+    completedAt: 333
 }, {
     text: 'Third test todo'
 }];
@@ -104,6 +106,45 @@ describe('POST /todos', () => {
                     done();
                 }).catch((err) => done(err));
             });
+    });
+});
+
+// PATCH: /todos/:id
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', (done) => {
+        var hexID = todos[0]._id.toHexString();
+        var text = 'This should be the new text';
+        request(app)
+            .patch(`/todos/${ hexID }`)
+            .send({
+                completed: true,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(true);
+                expect(res.body.todo.completedAt).toBeA('number');
+            })
+            .end(done);
+    });
+
+    it('should clear completedAt when todo is not completed', (done) => {
+        var hexID = todos[1]._id.toHexString();
+        var text = 'This should be the new text!';
+        request(app)
+            .patch(`/todos/${ hexID }`)
+            .send({
+                completed: false,
+                text
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(text);
+                expect(res.body.todo.completed).toBe(false);
+                expect(res.body.todo.completedAt).toNotExist();
+            })
+            .end(done);
     });
 });
 
