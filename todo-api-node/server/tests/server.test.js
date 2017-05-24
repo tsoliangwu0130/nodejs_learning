f66475bea355c896ee6c65f0ccd14bcdaf2abdb9
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-23 18:30:24 -0700

    Impletment unit test for POST /users/login

--- todo-api-node/server/tests/server.test.js
+++ todo-api-node/server/tests/server.test.js
@@ -221,7 +221,7 @@ describe('POST /users', () => {
                     expect(user).toExist();
                     expect(user.password).toNotBe(password);
                     done(err);
-                });
+                }).catch((err) => done(err));
             });
     });
 
@@ -245,5 +245,55 @@ describe('POST /users', () => {
             })
             .expect(400)
             .end(done);
+    });
+});
+
+// POST: /users/login
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens[0]).toInclude({
+                        access: 'auth',
+                        token: res.headers['x-auth']
+                    });
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+
+    it('should reject invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({
+                email: users[1].email,
+                password: users[1].password + 'wrong'
+            })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err) => {
+                if (err) {
+                    return done(err);
+                }
+                User.findById(users[1]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((err) => done(err));
+            });
     });
 });
