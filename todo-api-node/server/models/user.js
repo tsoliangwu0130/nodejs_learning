cf1cfd3060c58600aa3d394fc49a431b64138196
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-05-23 18:54:40 -0700

    Impletment DELETE: /users/me/token

--- todo-api-node/server/models/user.js
+++ todo-api-node/server/models/user.js
@@ -53,6 +53,16 @@ UserSchema.methods.generateAuthToken = function () {
     });
 };
 
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: { token }
+        }
+    });
+};
+
 // module methods
 UserSchema.statics.findByToken = function (token) {
     var User = this;
