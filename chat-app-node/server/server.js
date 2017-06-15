75681388cb97ca25047e69078b25d5463571e6ce
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-06-15 00:08:18 -0700

    Send message to room only

--- chat-app-node/server/server.js
+++ chat-app-node/server/server.js
@@ -38,13 +38,22 @@ io.on('connection', (socket) => {
 
     // event listener: createMessage
     socket.on('createMessage', (message, callback) => {
-        io.emit('newMessage', generateMessage(message.from, message.text));
+        var user = users.getUser(socket.id);
+
+        if (user && isRealString(message.text)) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
+
         callback();
     });
 
     // event listener: createLocationMessage
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+        var user = users.getUser(socket.id);
+
+        if (user) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+        }
     });
 
     socket.on('disconnect', () => {
