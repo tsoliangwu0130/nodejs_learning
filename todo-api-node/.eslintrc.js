92d9f8608c42d3b28906ddf15725a18a37df1a19
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-07-01 12:22:30 -0700

    Add ecmaVersion: 8

--- todo-api-node/.eslintrc.js
+++ todo-api-node/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "extends": "eslint:recommended",
     "parserOptions": {
         "ecmaFeatures": {
+            "ecmaVersion": 8,
             "experimentalObjectRestSpread": true,
             "jsx": true
         },
