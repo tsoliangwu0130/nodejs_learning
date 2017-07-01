0d739aeb5e1f6a1862125b1656554bb0494c1838
Tso-Liang Wu <tsoliangwu0130@gmail.com>
2017-07-01 13:05:06 -0700

    Modify functions use async/await

--- todo-api-node/server/server.js
+++ todo-api-node/server/server.js
@@ -132,26 +132,27 @@ app.post('/users', (req, res) => {
     });
 });
 
+// **** async verstion ****
 // POST: /users/login { email, password }
-app.post('/users/login', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
-
-    User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) => {
-            res.header('x-auth', token).send(user);
-        });
-    }).catch((err) => {
-        res.status(400).send(err);
-    });
+app.post('/users/login', async (req, res) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 // DELETE: /users/me/token
-app.delete('/users/me/token', authenticate, (req, res) => {
-    req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async (req, res) => {
+    try {
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    }, () => {
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 app.listen(port, () => {
